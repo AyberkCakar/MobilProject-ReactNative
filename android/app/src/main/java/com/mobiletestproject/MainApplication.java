d35eea4fc99522bad3dab6d8b6fde35e987eec47
AyberkCakar <ayberkcakar35@gmail.com>
2020-05-11 04:51:30 +0300

    camera added.

--- android/app/src/main/java/com/mobiletestproject/MainApplication.java
+++ android/app/src/main/java/com/mobiletestproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.mobiletestproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.sensors.RNSensorsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new MapsPackage(),
             new RNSensorsPackage(),
             new RNDeviceInfo(),
--- src/screens/Photo/index.js
+++ src/screens/Photo/index.js
@@ -1,12 +1,98 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity, CameraRoll, PermissionsAndroid, Platform} from 'react-native';
+
+import {RNCamera} from 'react-native-camera';
 
 export default class Photo extends Component {
-  render() {
-    return (
-      <View></View>
-    );
-  }
+    requestWriteExternalStoragePermission = async () => {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+            );
+
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                console.log('You can use the camera');
+            } else {
+                console.log('Camera permission denied');
+            }
+        } catch (err) {
+            console.warn(err);
+        }
+    };
+
+    takePhoto = async () => {
+        if (this.camera) {
+
+            const options = {
+                quality: 0.7,
+                base64: true
+            };
+
+            if (Platform.OS === 'android' && Platform.Version > 22) {
+                await this.requestWriteExternalStoragePermission();
+            }
+
+            const data = await this.camera.takePictureAsync(options);
+
+            CameraRoll
+                .saveToCameraRoll(data.uri, 'photo')
+                .then(() => {
+                    alert('Success')
+                })
+                .catch(e => {
+                    alert('err')
+                    console.log(e);
+                })
+        }
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {
+                    <RNCamera
+                        ref={ref => {
+                            this.camera = ref;
+                        }}
+                        type={RNCamera.Constants.Type.back}
+                        style={styles.preview}
+                    />
+
+                }
+                <View style={styles.bottomController}>
+                    <TouchableOpacity
+                        onPress={this.takePhoto}
+                        style={styles.snapButton}>
+                        <Text style={styles.snapText}>SNAP</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#000'
+    },
+    preview: {
+        flex: 1
+    },
+    bottomController: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    snapButton: {
+        backgroundColor: '#fff',
+        flex: 1,
+        borderRadius: 6,
+        padding: 15,
+        margin: 15,
+    },
+    snapText: {
+        color: '#000',
+        textAlign: 'center'
+    }
+});
